468c6940dedbce50b9dc965b45320321e7b91fe1
unknown <prathvisingh1604@gmail.com>
2023-02-22 18:53:40 +0530

    changes

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import {signupUser,loginUser} from '../controllers/user-controller.js'
 import { uploadImage } from '../controllers/uploadImage-controller.js';
-import upload from '../utils/upload.js'
+// import upload from '../utils/upload.js'
 import {createPost,getAllPosts,getPost,updatePost,deletePost} from '../controllers/post-controller.js'
 import { authenticateToken } from '../controllers/jwt-auth-controller.js';
 import UpdatePost from '../../client1/src/Component/Create/updatePost.js';
@@ -12,7 +12,7 @@ const router =express();
 
 router.post('/signup',signupUser);
 router.post('/login',loginUser)
-router.post('/file/upload',upload.single('file'),uploadImage);
+//router.post('/file/upload',upload.single('file'),uploadImage);
 router.post('/create',authenticateToken,createPost);
 router.get('/posts',authenticateToken,getAllPosts);
 router.get('/post:id',authenticateToken,getPost);
--- utils/upload.js
+++ utils/upload.js
@@ -1,29 +1,29 @@
-import multer  from 'multer'
-import { GridFsStorage} from 'multer-gridfs-storage'
-import dotenv from 'dotenv'
+// import multer  from 'multer'
+// import { GridFsStorage} from 'multer-gridfs-storage'
+// import dotenv from 'dotenv'
 
-dotenv.config();
+// dotenv.config();
 
-const username=process.env.DB_USERNAME;
-const userpassword=process.env.DB_PASSWORD;
+// const username=process.env.DB_USERNAME;
+// const userpassword=process.env.DB_PASSWORD;
 
-const storage =new GridFsStorage({
+// const storage =new GridFsStorage({
    
-    url:`mongodb+srv://${username}:${userpassword}@cluster0.r5lt5ic.mongodb.net/blogapp?retryWrites=true&w=majority`,
-    options : {useNewUrlParser : true},
-    file : (request,file) => {
+//     url:`mongodb+srv://${username}:${userpassword}@cluster0.r5lt5ic.mongodb.net/blogapp?retryWrites=true&w=majority`,
+//     options : {useNewUrlParser : true},
+//     file : (request,file) => {
       
-        const match = ['image/png','image/jpeg'];
+//         const match = ['image/png','image/jpeg'];
        
-        if(match.indexOF(file.memeType)===-1){
+//         if(match.indexOF(file.memeType)===-1){
            
-            return `${Date.now()}-blog-${file.originalname}`
-        }
-        return {
-            bucketName:"photos",
-            filname:`${Date.now()}-blog-${file.originalname}`
-        }
-    }
-})
+//             return `${Date.now()}-blog-${file.originalname}`
+//         }
+//         return {
+//             bucketName:"photos",
+//             filname:`${Date.now()}-blog-${file.originalname}`
+//         }
+//     }
+// })
 
-export default multer({storage});
+// export default multer({storage});
